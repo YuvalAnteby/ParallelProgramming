4cdfcb0fa07a3b3cc6753b3f160397a223e1463c
yuval <yuvala2001@gmail.com>
2026-03-26 21:40:52 +0200

    ex1/task4 initial files

---
+++ ex1/task4/formula1.c
@@ -0,0 +1,14 @@
+/* 212152896 Yuval Anteby */
+
+#include <xmmintrin.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pmmintrin.h>
+
+#define MAX_STR 255
+
+float formula1(float *x, unsigned int length) {
+
+}
+
